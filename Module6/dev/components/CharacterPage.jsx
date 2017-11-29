bc938a527e498bb390eb6231dab062936e39a96c
Gwen Faraday <gwenfaraday@gmail.com>
2017-11-29 17:03:30 -0500

    updated character page to make API request

--- Module6/dev/components/CharacterPage.jsx
+++ Module6/dev/components/CharacterPage.jsx
@@ -1,6 +1,36 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import axios from 'axios';
 
 class CharacterPage extends Component {
+  constructor () {
+    super();
+    this.state = {
+      character: null
+    };
+
+    this.fetchCharacter = this.fetchCharacter.bind(this);
+  }
+
+  componentDidMount () {
+    this.fetchCharacter();
+  }
+
+  fetchCharacter () {
+    var _this = this;
+    var id = this.props.match.params.id;
+    axios.get(`https://www.anapioficeandfire.com/api/characters/${id}`)
+      .then(function (res) {
+        _this.setState({
+          character: res.data
+        });
+        console.log(res);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
   render () {
     return (
       <div className="character-page-container">
@@ -10,4 +40,4 @@ class CharacterPage extends Component {
   };
 }
 
-export default CharacterPage;
+export default withRouter(CharacterPage);
